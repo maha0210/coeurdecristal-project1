344e61baf8a4e3364d6819f5add12c6c3186e4bf
maha0210 <maha.almajdoub@gmail.com>
2024-12-25 15:55:11 +0100

    Delete New Folder With Items directory

--- New Folder With Items/detailcheckout.js
+++
@@ -1,9 +0,0 @@
-//The following 7 lines of codes was adapted by Blackbox AI Accessed 2023-12-04
-function showPopup() {
-  document.querySelector(".popupmodal").style.display = "block";
-}
-function hidePopup(event) {
-  if (event.target === document.querySelector(".popupmodal")) {
-    document.querySelector(".popupmodal").style.display = "none";
-  }
-}
